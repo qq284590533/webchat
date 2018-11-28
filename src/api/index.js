3643e8c622e4280fdf26ab888dfc21d805162082
chenlin <284590533@qq.com>
2018-11-28 14:52:58 +0800

    打包

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ import ajax from '../common/ajax'
 export function login(params) {
 	console.log(params)
     return ajax({
+		// url: 'http://api.yichatsystem.com/api/user/login',
 		url: '/api/user/login',
 		method: 'post',
 		dataType: "jsonp",
@@ -14,6 +15,7 @@ export function login(params) {
 //添加好友
 export function addFriends(params) {
 	return ajax({
+		// url: 'http://api.yichatsystem.com/api/user/setFriends',
 		url: '/api/user/setFriends',
 		method: 'post',
 		dataType: "jsonp",
@@ -24,6 +26,7 @@ export function addFriends(params) {
 //获取好友
 export function getFriendsList(params) {
 	return ajax({
+		// url: 'http://api.yichatsystem.com/api/user/getContactList',
 		url: '/api/user/getContactList',
 		method: 'post',
 		dataType: "jsonp",
@@ -34,6 +37,7 @@ export function getFriendsList(params) {
 //获取群列表
 export function getGroupList(params) {
 	return ajax({
+		// url: 'http://106.14.241.187:19080/rest/adhoc/muc@muc.app.im',
 		url: '/rest/adhoc/muc@muc.app.im',
 		method: 'post',
 		dataType: "jsonp",
